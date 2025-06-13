ee3387106fcba4f75ea370f387930e41a5f4e998
reannisance <annisanazalia00@gmail.com>
2025-06-13 09:09:06 +0700

    Update dashboard_kepatuhan_FILTER_STATUS1.py

--- dashboard_kepatuhan_FILTER_STATUS1.py
+++ dashboard_kepatuhan_FILTER_STATUS1.py
@@ -32,8 +32,10 @@ def hitung_kepatuhan(df, tahun_pajak):
     rata_rata_pembayaran = total_pembayaran / bulan_pembayaran.replace(0, 1)
     kepatuhan_persen = bulan_pembayaran / bulan_aktif.replace(0, 1) * 100
 
+    def klasifikasi(row):
+        bulan_aktif = row['bulan_aktif']
+        bulan_pembayaran = row['bulan_pembayaran']
 
-    def klasifikasi(kepatuhan):
             if bulan_aktif == 0 and bulan_pembayaran == 0:
                 return "Belum Aktif"
             elif bulan_pembayaran == bulan_aktif:
@@ -43,8 +45,9 @@ def hitung_kepatuhan(df, tahun_pajak):
             else:
                 return "Tidak Patuh"
 
+# Terapkan ke DataFrame
+    df['klasifikasi_kepatuhan'] = df.apply(klasifikasi, axis=1)
 
-    klasifikasi_kepatuhan = kepatuhan_persen.apply(klasifikasi)
 
     df["Total Pembayaran"] = total_pembayaran
     df["Bulan Aktif"] = bulan_aktif
